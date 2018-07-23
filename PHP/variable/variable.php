a29812ea17e87f426010c0e547b35cc08be5c153
medleew <nadir.medlee@gmail.com>
2018-07-23 10:46:04 +0200

    Ajout exo variable

---
+++ PHP/variable/variable.php
@@ -0,0 +1,9 @@
+<?php 
+$name = 'Nadir';
+$surname = 'Mohamed Ali';
+$age = 24;
+$hungry = true;
+$eyeColor = 'Marron';
+$familly = array(0 => 'Father', 1 => 'Mother', 2 => 'Sister1', 3 => 'Sister2');
+echo ' Hello, je suis '. $name . ' ' . $surname . " et j'ai " . $age;
+?>

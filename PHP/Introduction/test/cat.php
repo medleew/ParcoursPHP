109ae99009f9217d30f6ee2625316eb9d90893c1
medleew <nadir.medlee@gmail.com>
2018-07-23 10:17:41 +0200

    fin de l'introduction à php avec petit exo

---
+++ PHP/Introduction/test/cat.php
@@ -0,0 +1,6 @@
+<?php
+echo "Welcome back!<br/>";
+echo '<img src="https://camo.githubusercontent.com/3e760dfa4bb6167c0fb27b6a0fab2ac518efc457/687474703a2f2f6d65646961312e67697068792e636f6d2f6d656469612f3335674e67366f3248596a53672f67697068792e676966'.$cat.'"> <br/>'; 
+echo '<a href="index.php">lien vers une autre page</a>'
+
+?>
--- PHP/Introduction/test/index.php
+++ PHP/Introduction/test/index.php
@@ -1,3 +1,6 @@
 <?php
-echo "Hello!";
+echo "Hello! <br/>";
+echo '<img src="https://camo.githubusercontent.com/f9b43986af515ac850dfa82609fb2ed6938bf1ac/687474703a2f2f6d65646961302e67697068792e636f6d2f6d656469612f6e734d5068574b3662667848712f67697068792e676966'.$chaton.'"> <br/>'; 
+echo '<a href="cat.php">lien vers une autre page</a>'
+
 ?>

6fe1e0dab992d8ed7ab1a0ef45d129cd1ce00e89
medleew <nadir.medlee@gmail.com>
2018-07-24 16:53:04 +0200

    debut de l'exo generateur d'excuses

---
+++ PHP/excuse_generator/excuse.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="excuse.php" method="get">
+        Nom:  <input type="text"> <br/>
+        Sexe: M<input type="checkbox" name="male"> F <input type="checkbox" name="female">
+        institutrice: <input type="text">
+
+    </form>
+    <?php
+
+    ?>
+</body>
+</html>
